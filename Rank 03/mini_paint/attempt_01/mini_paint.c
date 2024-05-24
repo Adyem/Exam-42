bb04753f0c5ec76722a509e9fec4f9cc03d73f8b
Adyem <Bastianvangenechten@hotmail.com>
2024-05-24 14:36:51 +0200

    adding

--- Rank 03/mini_paint/attempt_01/mini_paint.c
+++ Rank 03/mini_paint/attempt_01/mini_paint.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <math.h>
 
 int ft_strlen(char *string)
 {
-	int i;
+	int	i;
 
 	if (!string)
 		return (0);
@@ -14,16 +15,16 @@ int ft_strlen(char *string)
 	return (i);
 }
 
-int ft_error(char *msg, int error)
+int ft_error(char *msg, int rvalue)
 {
 	write(1, msg, ft_strlen(msg));
-	return (error);
+	return (rvalue);
 }
 
-char **ft_make_canves(FILE *file)
+char	**ft_make_canves(FILE *file)
 {
-	float	width;
 	float	hight;
+	float	width;
 	char	symbol;
 	int		number;
 	int		i;
@@ -33,7 +34,7 @@ char **ft_make_canves(FILE *file)
 	number = fscanf(file, "%f %f %c\n", &width, &hight, &symbol);
 	if (number != 3)
 		return (NULL);
-	if (width > 300 || hight > 300 || width <= 0 || hight <= 0)
+	if (width <= 0 || hight <= 0 || width > 300 || hight > 300)
 		return (NULL);
 	canves = (char **)malloc((hight + 1) * sizeof(char *));
 	if (!canves)
@@ -41,10 +42,10 @@ char **ft_make_canves(FILE *file)
 	i = 0;
 	while (i < hight)
 	{
+		j = 0;
 		canves[i] = (char *)malloc((width + 1) * sizeof(char));
 		if (!canves[i])
 			return (NULL);
-		j = 0;
 		while (j < width)
 		{
 			canves[i][j] = symbol;
@@ -57,59 +58,65 @@ char **ft_make_canves(FILE *file)
 	return (canves);
 }
 
-int	ft_check(float float_i, float float_j, float s_width, float s_hight, float width, float hight)
+int	ft_check_pos(float floati, float floatj, float s_width, float s_hight, float radius)
 {
-	if (float_i < s_hight || float_j < s_width || float_i > s_hight + hight || float_j > s_width + width)
-		return (0);
-	if ((float_i - s_hight < 1.0) || (s_hight + hight - float_i < 1.0)
-			|| float_j - s_width < 1.0 || (s_width + width - float_j < 1.0))
+	float	distance;
+
+	distance = sqrtf(powf(floati - s_hight, 2.) + powf(floatj - s_width, 2.));
+	if (distance <= radius)
+	{
+		printf("checking\n");
+		if (radius - distance < 1.0)
 			return (2);
-	return (1);
+		return (1);
+	}
+	return (0);
 }
 
-int	ft_draw_sqaure(FILE *file, char **canves)
+int	ft_draw_circle(FILE *file, char **canves)
 {
-	float	width;
-	float	hight;
-	float	s_width;
+	float	radius;
 	float	s_hight;
-	float	float_i;
-	float	float_j;
+	float	s_width;
+	char	symbol;
+	char	filled;
+	int		number;
 	int		i;
 	int		j;
-	char	filled;
-	char	symbol;
-	int		number;
-	int		check;
+	float	floati;
+	float	floatj;
+	int		pos;
 
-	while ((number = fscanf(file, "%c %f %f %f %f %c\n", &filled, &s_width, &s_hight, &width, &hight, &symbol)) == 6)
+	while ((number = fscanf(file, "%c %f %f %f %c\n", &filled, &s_width, &s_hight, &radius, &symbol)) == 5)
 	{
-		if (width <= 0.0 || hight <= 0.0 || (filled != 'r' && filled != 'R'))
+		if (radius < 0.0 || (filled != 'c' && filled != 'C'))
 			return (1);
 		i = 0;
+		printf("test\n");
 		while (canves[i])
 		{
 			j = 0;
 			while (canves[i][j])
 			{
-				float_i = (float)i;
-				float_j = (float)j;
-				check = ft_check(float_i, float_j, s_width, s_hight, width, hight);
-				if ((check == 1 || check == 2) && filled == 'R')
+				floati = (float)i;
+				floatj = (float)j;
+				pos = ft_check_pos(floati, floatj, s_width, s_hight, radius);
+				if ((pos == 1 || pos == 2) && filled == 'C')
 					canves[i][j] = symbol;
-				else if (check == 2 && filled == 'r')
+				else if (pos == 2 && filled == 'c')
 					canves[i][j] = symbol;
 				j++;
 			}
 			i++;
 		}
 	}
-	if (number != -1 && number != 6)
+	printf("number = %i", number);
+	if (number != -1 && number != 5)
 		return (1);
 	return (0);
 }
 
-void	ft_draw_canves(char **canves)
+void	ft_print_canves(char **canves)
 {
 	int	i;
 	int	j;
@@ -134,15 +141,13 @@ int main(int argc, char **argv)
 	char	**canves;
 
 	if (argc != 2)
-		return (ft_error("Error: argument\n", 1));
+		return (ft_error("Error: Argument\n", 1));
 	file = fopen(argv[1], "r");
 	if (!file)
 		return (ft_error("Error: Operation file corrupted\n", 1));
 	canves = ft_make_canves(file);
 	if (!canves)
 		return (ft_error("Error: Operation file corrupted\n", 1));
-	if (ft_draw_sqaure(file, canves))
-		return (ft_error("Error: Operation file corrupted\n", 1));
-	ft_draw_canves(canves);
+	ft_print_canves(canves);
 	return (0);
 }
