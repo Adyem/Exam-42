03149f9a02b4a6181746fc57d189a2470d077a08
Bastian Vangenechten <Bastianvangenechten@hotmail.com>
2024-09-10 09:24:48 +0200

    adding

--- 42_EXAM-2.1/success/cpp_module00/Warlock.cpp
+++ 42_EXAM-2.1/success/cpp_module00/Warlock.cpp
@@ -1,10 +1,10 @@
-#include "Warlock.hpp"
 #include <string>
 #include <iostream>
+#include "Warlock.hpp"
 
 Warlock::Warlock() {}
 
-Warlock &Warlock::operator=(const Warlock &other)
+Warlock & Warlock::operator=(const Warlock &other)
 {
 	if (this != &other)
 	{
@@ -19,7 +19,7 @@ Warlock::Warlock(const Warlock &other)
 	*this = other;
 }
 
-Warlock::Warlock(const std::string name, const std::string title) : _name(name), _title(title)
+Warlock::Warlock(const std::string &name, const std::string &title) : _name(name), _title(title)
 {
 	std::cout << _name << ": This looks like another boring day." << std::endl;
 }
@@ -39,12 +39,12 @@ const std::string &Warlock::getTitle() const
 	return (this->_title);
 }
 
-void Warlock::setTitle(const std::string title)
+void Warlock::setTitle(const std::string &title)
 {
-	this->_name = title;
+	this->_title = title;
 }
 
 void Warlock::introduce() const
 {
-	std::cout << _name << ": I am " << _name << ", " << _title << "!" << std::endl;
+	std::cout << _name << ": i am " << _name << ": " << _title << "!" << std::endl;
 }
--- 42_EXAM-2.1/success/cpp_module00/Warlock.hpp
+++ 42_EXAM-2.1/success/cpp_module00/Warlock.hpp
@@ -9,16 +9,16 @@ class Warlock
 		std::string _name;
 		std::string _title;
 		Warlock();
+		Warlock(const Warlock &other);
 		Warlock &operator=(const Warlock &other);
-		Warlock (const Warlock &other);
 
 	public:
-		Warlock(const std::string name, const std::string title);
+		Warlock(const std::string &name, const std::string &title);
 		~Warlock();
 		const std::string &getName() const;
 		const std::string &getTitle() const;
-		void setTitle(const std::string title);
+		void setTitle(const std::string &title);
 		void introduce() const;
-};
+};  
 
 #endif
